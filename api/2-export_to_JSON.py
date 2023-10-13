016319da79cf7f6bf4bf01840672f135b4b0e31f
Othmane9011 <124583251+Othmane9011@users.noreply.github.com>
2023-10-13 12:46:59 +0200

    Update 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-'''
-Python script that exports data in the JSON format
-'''
+"""Extends the 0-gather_data_from_an_API to export data in json format"""
+
 import json
 import requests
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    get_emp_id = sys.argv[1]
-    user_url = (f'https://jsonplaceholder.typicode.com/users/{get_emp_id}')
-    get_emp_data = requests.get(user_url).json()
-    todos_url = (
-        f'https://jsonplaceholder.typicode.com/todos?userId={get_emp_id}'
-    )
-    get_emp_tasks = requests.get(todos_url).json()
+if __name__ == '__main__':
+    user_page = requests.get(
+            "https://jsonplaceholder.typicode.com/users/{}".
+            format(argv[1])).json()
+    todo_page = requests.get(
+            "https://jsonplaceholder.typicode.com/todos?userId={}".
+            format(argv[1])).json()
+    username = user_page.get("username")
+    user_id = argv[1]
 
-    tasks_list = []
-    for task in get_emp_tasks:
+    tasks = []
+    for task in todo_page:
         task_dict = {}
-        task_dict["task"] = task.get("title")
-        task_dict["completed"] = task.get("completed")
-        task_dict["username"] = get_emp_data.get("username")
-        tasks_list.append(task_dict)
-
-    tasks_json = {}
-    tasks_json[get_emp_id] = tasks_list
-
-    with open("{}.json".format(get_emp_id), 'w') as jsonfile:
-        json.dump(tasks_json, jsonfile)
+        task_dict["task"] = task.get('title')
+        task_dict["completed"] = task.get('completed')
+        task_dict["username"] = username
+        tasks.append(task_dict)
+        todos = {}
+        todos[user_id] = tasks
+        with open("{}.json".format(user_id), 'w') as jsonfile:
+            json.dump(todos, jsonfile)
