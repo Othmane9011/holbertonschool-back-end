b68e910eff4ff8271cd0b0915a248532b27841f5
Othmane9011 <124583251+Othmane9011@users.noreply.github.com>
2023-10-13 12:46:42 +0200

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-'''
-A script to export data in the CSV format.
-'''
+"""Extends the 0-gather_data_from_an_API to export data in CSV format"""
 
 import csv
 import requests
-import sys
-if __name__ == "__main__":
-    get_emp_id = sys.argv[1]
-    user_url = f'https://jsonplaceholder.typicode.com/users/{get_emp_id}'
-    get_emp_data = requests.get(user_url).json()
+from sys import argv
 
-    todos_url = (
-        f'https://jsonplaceholder.typicode.com/todos?userId={get_emp_id}'
-    )
-    get_emp_tasks = requests.get(todos_url).json()
+if __name__ == '__main__':
+    user_page = requests.get(
+            "https://jsonplaceholder.typicode.com/users/{}".
+            format(argv[1])).json()
+    todo_page = requests.get(
+            "https://jsonplaceholder.typicode.com/todos?userId={}".
+            format(argv[1])).json()
 
-    with open("{}.csv".format(get_emp_id), 'w', newline='') as csvfile:
-        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in get_emp_tasks:
-            taskwriter.writerow([get_emp_id, get_emp_data.get(
-                'username'), task.get('completed'), task.get('title')])
+    with open("{}.csv".format(argv[1]), 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todo_page:
+            csv_writer.writerow([int(argv[1]), user_page.get('username'),
+                                task.get('completed'),
+                                task.get('title')])
